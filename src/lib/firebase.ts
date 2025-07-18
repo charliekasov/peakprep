44690e0dd23b9baea9047bd8d87bcedb8098ec23
Charlie Kasov <CharlieKasov@gmail.com>
2025-07-18 19:41:11 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/a
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "YOUR_API_KEY",
+  authDomain: "tutorflow-ivaba.firebaseapp.com",
+  projectId: "tutorflow-ivaba",
+  storageBucket: "tutorflow-ivaba.appspot.com",
+  messagingSenderId: "155331393444",
+  appId: "1:155331393444:web:603173740269389771191a"
 };
 
 // Initialize Firebase
